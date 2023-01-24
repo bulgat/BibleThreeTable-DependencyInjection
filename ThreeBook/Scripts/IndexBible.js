0482b18f4afd52a20e31148456b27d123d038b37
kol <sirbulgat@gmail.com>
2023-01-24 18:11:03 +0300

    add array

--- ThreeBook/Scripts/IndexBible.js
+++ ThreeBook/Scripts/IndexBible.js
@@ -1,16 +1,26 @@
 ﻿
 function GetFirstBook() {
     let result;
-    const kol = async () => {
+    let getKol = async () => {
        
-        const response = await fetch("./FirstBook").then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log("book = ", data);
-            result = data;
-         });;
-        return response;
+        const response = await fetch("/Home/FirstBook")
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log("book = ", data);
+                result = data;
+                return result;
+            }).catch(err => console.log("ERROR = ", err));
+        
     }
-    kol();
-
+    getKol();
+   
+    var kol = [3, 5, 7, 9];
+    var t = kol.find(a => a == 7);
+    var tt = [...kol, 999];
+    var ttt = kol.push(666);
+    console.log("00000 = ", t);
+    console.log("00001 = ", tt);
+    console.log("00002 = ", ttt);
+    console.log("00003 = ", kol);
 }
