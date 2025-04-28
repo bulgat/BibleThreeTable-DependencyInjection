090a8d9e7434cf28483d350cba3d3ded726a25c9
Nikolay K <sirbulgat@gmail.com>
2025-04-28 22:36:17 +0300

    add request post

---
+++ ThreeBook/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThreeBook.Controllers
+{
+    public class GameController: Controller
+    {
+        [HttpGet]
+        public string GetNumber()
+        { 
+        return "stop";
+        }
+        [HttpPost]
+        public string GetNum()
+        {
+            return "This is Post";
+        }
+    }
+}
--- ThreeBook/Models/TypesBook.cs
+++ ThreeBook/Models/TypesBook.cs
@@ -26,5 +26,6 @@ namespace ThreeBook.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TitleBook> TitleBook { get; set; }
+    
     }
 }
